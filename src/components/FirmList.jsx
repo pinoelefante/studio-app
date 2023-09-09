407443f8220805b6db0bd20224ccea8ac541a2a6
Giuseppe Elefante <pinoelefante@hotmail.it>
2023-09-09 12:54:00 +0200

    mostra i servizi attivi nell'elenco delle aziende

--- src/components/FirmList.jsx
+++ src/components/FirmList.jsx
@@ -14,6 +14,10 @@ class FirmList extends Component {
 			label: "Funzioni",
 			renderContent: (item) => <OpenFirmButton firm={item} />,
 		},
+		{
+			keys: "services", label: "Servizi attivi", 
+			renderContent: (item) => <FirmListEnabledServices firm={item} />
+		}
 	];
 	async componentDidMount() {
 		const { data: firms } = await firmApi.getFirms();
@@ -31,5 +35,33 @@ class FirmList extends Component {
 		);
 	}
 }
+
+export const FirmListEnabledServices = ({ firm }) => {
+	const services = firm.services;
+	if (!services || (!services.includes("FATTURE") && !services.includes("CORRISPETTIVI") && !services.includes("BOLLO"))) {
+		return <></>
+	}
+	return <>
+		<table>
+			<tr>
+				<td>{services.includes("FATTURE") ? renderSquare("F", "Fatture") : renderSquareEmpty()}</td>
+				<td>{services.includes("CORRISPETTIVI") ? renderSquare("C", "Corrispettivi") : renderSquareEmpty()}</td>
+				<td>{services.includes("BOLLO") ? renderSquare("B", "Bollo") : renderSquareEmpty()}</td>
+			</tr>
+		</table>
+	</>
+
+	function renderSquare(text, hoverText) {
+		return <span style={{ padding: "5px" , minWidth: "80px", minHeight: "80px", maxWidth: "80px", maxHeight: "80px", border: "2px solid green", borderRadius: "4px" }} title={hoverText}>
+			{text}
+		</span>
+	}
+
+	function renderSquareEmpty() {
+		return <span style={{ padding: "5px" , minWidth: "80px", minHeight: "80px", maxWidth: "80px", maxHeight: "80px", }}>
+			&nbsp;
+		</span>
+	}
+};
 
 export default FirmList;
